350072771532b28ce269ac8ef648fb49ecc64377
Erin Meyers <erinmeyers88@gmail.com>
2015-12-07 15:30:36 -0700

    commented out facebook service in routes

--- core/server/features/user/user.server.routes.js
+++ core/server/features/user/user.server.routes.js
@@ -1,6 +1,6 @@
 var Ctrl = require("./user.server.controller");
 var config = require('../../config/config');
-var facebook = require("../../services/facebookService")(config.facebook.clientID, config.facebook.clientSecret);
+// var facebook = require("../../services/facebookService")(config.facebook.clientID, config.facebook.clientSecret);
 
 module.exports = function (app) {
 

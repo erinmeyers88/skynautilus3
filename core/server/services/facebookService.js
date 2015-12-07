6310acc407e1d50012ed2e2416af110e68a728fa
Erin Meyers <erinmeyers88@gmail.com>
2015-12-07 15:29:24 -0700

    commented out facebook service

--- core/server/services/facebookService.js
+++ core/server/services/facebookService.js
@@ -1,23 +1,23 @@
-var OAuth = require("OAuth").OAuth2;
+// var OAuth = require("OAuth").OAuth2;
 
-var Facebook = function (facebookKey, facebookSecret) {
+// var Facebook = function (facebookKey, facebookSecret) {
 	
-	var key = facebookKey;
-	var secret = facebookSecret;
+// 	var key = facebookKey;
+// 	var secret = facebookSecret;
 	
 	
-	var oauth = new OAuth(key, secret, "https://graph.facebook.com", null, "oauth2/token", null);
+// 	var oauth = new OAuth(key, secret, "https://graph.facebook.com", null, "oauth2/token", null);
 	
-	var getImage = function (userKey, done) {
-		oauth.get("https://graph.facebook.com/v2.5/me/picture?redirect=false", userKey, function (err, results, res) {
-			results = JSON.parse(results);
-			done(results.data);
-		});	
-	};
+// 	var getImage = function (userKey, done) {
+// 		oauth.get("https://graph.facebook.com/v2.5/me/picture?redirect=false", userKey, function (err, results, res) {
+// 			results = JSON.parse(results);
+// 			done(results.data);
+// 		});	
+// 	};
 	
-	return {
-		getImage: getImage
-	};
-};
+// 	return {
+// 		getImage: getImage
+// 	};
+// };
 
-module.exports = Facebook;
+// module.exports = Facebook;
